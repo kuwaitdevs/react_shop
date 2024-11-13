ddf77babc5e70994aed78c9735d3bbd6999d532a
Anas Najaa <spidernet12@gmail.com>
2024-11-13 17:07:57 +0300

    React Shop Page 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+
 import './App.css';
+import img_store_front from './img_store_front.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='text-center'>
+      <h1>Cookies and beyond</h1>
+      <p>Where cookies brother gather</p>
+      <img className='w-100' alt='image-store-front' src={img_store_front} />
     </div>
   );
 }
