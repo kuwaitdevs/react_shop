1c87c82f6c24ce4590fa95c653421471160a3ab7
Anas Najaa <spidernet12@gmail.com>
2024-11-14 09:58:11 +0300

    Shop Page 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 
 import './App.css';
 import img_store_front from './img_store_front.png';
+import products_data from './products_data';
 
 function App() {
+
+  const productsList = products_data.map(product => {
+    return (
+      <div className='product-item' key={product.id}>
+        <img className='product-item-img' src={product.image} alt={product.name} />
+        <span className='mb-2'>{product.name}</span>
+        <span className='mb-2'>{product.price} KWD</span>
+      </div>
+    );
+  })
+
   return (
     <div className='text-center'>
       <h1>Cookies and beyond</h1>
       <p>Where cookies brother gather</p>
       <img className='w-100' alt='image-store-front' src={img_store_front} />
+      <div className='row'>
+        {productsList}
+      </div>
     </div>
   );
 }
---
+++ src/products_data.js
@@ -0,0 +1,26 @@
+import img_cookie_1 from './img_cookie_1.webp';
+import img_cookie_2 from './img_cookie_2.webp';
+import img_cookie_3 from './img_cookie_3.webp';
+
+const products_data = [
+    {
+        id: 1,
+        name: "Snickerdoodles",
+        image: img_cookie_1,
+        price: 20
+    },
+    {
+        id: 2,
+        name: "Chocolate Chip Cookies",
+        image: img_cookie_2,
+        price: 20
+    },
+    {
+        id: 3,
+        name: "Oatmeal Raisin Cookies",
+        image: img_cookie_3,
+        price: 20
+    },
+];
+
+export default products_data;
